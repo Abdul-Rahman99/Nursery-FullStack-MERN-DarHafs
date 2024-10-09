4064d146252ded8558750283a75bf9b5155dc0e2
Abdelrahman Ashraf Osman <s1751710231@std.aou.edu.eg>
2024-10-09 10:38:09 +0400

    Update index.js

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -6,7 +6,7 @@ const teacherRoute = require("./teacherRoute");
 
 const mountRoutes = (app) => {
   // app.use("/api/v1/users", userRoute);
-  // app.use("/api/v1/auth", authRoute);
+  // app.use("/api/v1/auth", authRoute) ;
   app.use("/api/v1/teacher", teacherRoute);
   app.use("/api/v1/student", studentRoute);
 };
